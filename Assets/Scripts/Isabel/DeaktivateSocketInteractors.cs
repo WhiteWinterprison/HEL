929b62c541819aaf1a09f49118c33c4600772abd
liSiGN <lisa.froehlichgabra@outlook.com>
2022-07-01 19:47:26 +0200

    Resource/ Buildings rework collider -add lisa scene stuff to Lobby scene -add Isabel scene stuff into Muliplayer scene -changed spawn position for  Script obj Vr&Cave Player -start with deactivate sockets script

---
+++ Assets/Scripts/Isabel/DeaktivateSocketInteractors.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeaktivateSocketInteractors : MonoBehaviour
+{
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    if (PhotonNetwork.InRoom)
+    //    {
+    //        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("VRSocket")) //find all players
+    //        {
+    //            if (!obj.GetComponent<PhotonView>().IsMine) //find the ones that are not this player
+    //            {
+    //                //and disable their cameras
+    //                foreach (Camera cam in obj.GetComponentsInChildren<Camera>())
+    //                {
+    //                    cam.enabled = false;
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
+
+}
