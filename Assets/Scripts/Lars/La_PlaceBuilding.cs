9a2edb41f6e43669539f4137c56fcf339228ce07
Lars Skogseide <lars.skogseide@stud.h-da.de>
2022-07-08 12:53:05 +0200

    Updated PlaceBuilding script to have hashtable with booleans checking which building is placed

--- Assets/Scripts/Lars/La_PlaceBuilding.cs
+++ Assets/Scripts/Lars/La_PlaceBuilding.cs
@@ -3,52 +3,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+
+using Hashtable = ExitGames.Client.Photon.Hashtable; //This line need to be on every script that uses the Hashtable!!
 
 public class La_PlaceBuilding : MonoBehaviour
-{
-    //public GameObject prefab;
-    //bool placedBuilding;
-  
+{ 
     Renderer rend;
-    public Material[] Materials;
+    public Material[] Materials; //place materials PutDown in element 0 and PickUp in element 1
+
+    //public bool buildingPlaced;
+
+    [SerializeField]
+    Hashtable buildingPlaced = new Hashtable() { { "building1", false }, { "building2", false }, { "building3", false }, { "building4", false }, { "building5", false }, { "building?", false } };
 
     //script for VR interaction
+
+    private void Awake()
+    {
+        //create "building" properties
+        buildingPlaced["building1"] = false;
+        buildingPlaced["building2"] = false;
+        buildingPlaced["building3"] = false;
+        buildingPlaced["building4"] = false;
+        buildingPlaced["building5"] = false;
+        buildingPlaced["building?"] = false;
+        PhotonNetwork.CurrentRoom.SetCustomProperties(buildingPlaced);
+    }
+
     void Start()
     {
+        //making sure the building renderer is recognized for changing material
         rend = GetComponent<Renderer>();
         rend.enabled = true;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (rend != null)
-        {
             if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Socket")
             {
-                rend.material = Materials[0];
+                rend.material = Materials[0]; //change material to be put down
+                SetBuildingToPlaced();
             }
-        }
-        else
-            Debug.LogWarning("Building Renderer not found, unable to change material");
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (rend != null)
-        {
             if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Socket")
             {
-                rend.material = Materials[1];
+                rend.material = Materials[1]; //change material to be picked up
             }
+    }
+
+    public void SetBuildingToPlaced()
+    {
+        if(this.gameObject.tag == "Building1")
+        {
+            buildingPlaced["building1"] = true;
+        }
+        if (this.gameObject.tag == "Building2")
+        {
+            buildingPlaced["building2"] = true;
+        }
+        if (this.gameObject.tag == "Building3")
+        {
+            buildingPlaced["building3"] = true;
+        }
+        if (this.gameObject.tag == "Building4")
+        {
+            buildingPlaced["building4"] = true;
+        }
+        if (this.gameObject.tag == "Building5")
+        {
+            buildingPlaced["building5"] = true;
         }
         else
-            Debug.LogWarning("Building Renderer not found, unable to change material");
+        {
+            buildingPlaced["building?"] = true;
+        }
+        PhotonNetwork.CurrentRoom.SetCustomProperties(buildingPlaced);
     }
 }
 
-//archive code
 
-// script for mouse interaction
+#region archive code
+
+// script for mouse interaction where grey building is replaced with textured building
 
 //RaycastHit hit;
 
@@ -80,3 +120,5 @@ void Update()
     }
 }
 */
+
+#endregion
